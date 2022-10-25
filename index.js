4ab79e9a726f777bab015f59b690eb846428e8f1
Jarno Varis <jarno.varis@gmail.com>
2022-10-25 17:17:00 +0300

    added post-data to morgan log. it also shows empty body for other requests, but it's okay I hope? could be easily fixed if needed

--- index.js
+++ index.js
@@ -27,7 +27,19 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+const morganCustom = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+})
+
+app.use(morganCustom)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
