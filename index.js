0d1b1931e3a2682bb992b68871e033a25c0a2f02
Jarno Varis <jarno.varis@gmail.com>
2022-10-27 19:40:01 +0300

    frontend & backend now works using mongoose, and mongoose is separated into person.js file

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
 // read .env file
 dotenv.config();
+const Person = require('./models/person');
 
 const app = express()
-
-const apiKey = process.env.MONGODB_API_KEY
-
-const url = `mongodb+srv://fullstack:${apiKey}@cluster0.ex0257f.mongodb.net/?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
 
 app.use(express.json())
 
@@ -65,33 +52,28 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  const alreadyExists = persons.some(person => person.name === body.name)
-
-  if (!body.name || !body.number || alreadyExists) {
-    return response.status(400).json({ 
-      error: 'name or number missing, or name already exists in phonebook' 
-    })
+  if (body.name === undefined) {
+    return response.status(404).json({ error: 'name missing' })
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
-    number: body.number  
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 // update existing
 app.post('/api/persons/:id', (request, response) => {
   // just update phone number
-  persons = persons.map(person => {
-    return person.id === Number(request.params.id) ? {...person, number: request.body.number}: person
-  })
 
-  response.json(persons.find(person => person.id === Number(request.params.id)))
+  Person.findByIdAndUpdate(request.params.id, {number: request.body.number}, {new: true})
+    .then(person => {
+      response.json(person)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -102,21 +84,18 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person
+    .findById(request.params.id)
+    .deleteOne(() => {
+      response.status(204).end()
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
 
-  if (person) {
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 const PORT = process.env.PORT || 3001
